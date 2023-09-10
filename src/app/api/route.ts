706383cce0a551e11067b3b54a88273ccb6754e8
simsimhan73 <simsimhan73@gmail.com>
2023-09-10 16:43:01 +0900

    l

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -6,23 +6,35 @@ import {kv} from '@vercel/kv';
 export async function GET(
   req: Request
 ) {  
-  let data = await kv.lrange('tas', 0, -1);
-  
-  return data ? NextResponse.json(data) : NextResponse.json({'status' : 'fail'})
+  try {
+    let data = await kv.lrange('tas', 0, -1);
+    
+    return data ? NextResponse.json(data) : NextResponse.json({'status' : 'fail'})
+  } catch (err) {
+    console.log(err)
+  }
 } 
 
 export async function PUT(
   req : Request
 ) {
+  try {
   kv.lpush('tas',req.body);
 
   return NextResponse.json({"status" : 201});
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 export async function DELETE(
   req : Request
 ) {
-  kv.lrem('tas',0, req.body)
+  try{
+    kv.lrem('tas',0, req.body)
 
-  return NextResponse.json({'status' : 200})
+    return NextResponse.json({'status' : 200})
+  } catch (err) {
+    console.log(err)
+  }
 }
